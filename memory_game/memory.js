17c1f961f12d80ecf5b0b739ec6c680a60e2c715
DT-1236 <wemascarinas@gmail.com>
2018-11-01 16:50:12 -0700

    Work done during orientation
    Heading home now
    
    Most of the logic should be complete. Need to add dynamic card colors, game startup menu, and finalize CSS

--- memory_game/memory.js
+++ memory_game/memory.js
@@ -1,3 +1,4 @@
+/*
 function shuffle(arr) {
     let currentIndex = arr.length;
     let newIndex, currentValue;
@@ -11,30 +12,150 @@ function shuffle(arr) {
 
     return arr;
 }
+*/
 
-function createCard(cardClass) {
+function createCard(cardNumber) {
     let card = document.createElement('div');
-    card.className = cardClass;
+    let front = document.createElement('div');
+    let back = document.createElement('div');
+    let topNum = document.createElement('div');
+    let botNum = document.createElement('div');
+
+    card.className = 'card' + cardNumber;
+    front.className = 'front';
+    back.className = 'back';
+    topNum.className = 'topNum';
+    botNum.className = 'botNum';
+
+    topNum.innerText = cardNumber;
+    botNum.innerText = cardNumber;
+
+    back.style.opacity = 1;
+    back.style.background = 'Olive';
+
+    front.appendChild(topNum);
+    front.appendChild(botNum);
+    card.appendChild(front);
+    card.appendChild(back);
 
     return card;
 }
 
-function createCards(cardClass, setSize) {
+function createCards(setSize) {
     let cards = [];
     for (let i = 0; i < setSize; i++) {
-        cards.push(createCard(cardClass + i))
+        cards.push(createCard(i))
     }
 
     return cards;
 }
 
 function createDeck(sets, setSize) {
-    let cards = [];
-    for (let i = 0; i < sets; i++) {
-        cards.splice(0, 0, ...createCards('card', setSize))
+    let deck = [];
+    for (let i = 0; i < setSize; i++) {
+        // Create a card for each set
+        deck.splice(0, 0, ...createCards(sets))
     }
 
-    return shuffle(cards);
+    return deck;
 }
 
-function setupMemoryGame(sets, setSize)
+function dealDeck(deck) {
+    removeDeck();
+    let surface = document.getElementById('surface');
+    for (let i = 0; i < deck.length; i++) {
+        deck[i].style.order = Math.floor(Math.random() * deck.length);
+        surface.appendChild(deck[i]);
+    }
+}
+
+function removeDeck() {
+    let surface = document.getElementById("surface");
+    while (surface.lastChild) {
+        surface.removeChild(surface.lastChild);
+    }
+}
+
+function revealCard(cardBack, selected) {
+    if (cardBack.style.opacity === 1) {
+        cardback.style.opacity = 0;
+        selected.push(cardBack);
+    }
+
+    return selected;
+}
+
+function selectionFailure() {
+    // Disables clicks for 1.5 seconds
+    document.addEventListener("click", clicker, true);
+    function clicker(e){
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    setTimeout(function() {document.removeEventListener('click', clicker, true);}, 1500);
+}
+
+function checkSuccess(selected) {
+    return selected.every(function(cardBack) {
+        return cardBack.parentElement.className === selected[0].parentElement.className;
+    });
+}
+
+function resolveSelected(selected, revealed) {
+    if (!checkSuccess(selected)) {
+        selectionFailure();
+        setTimeout(function() {
+            selected.forEach(function(cardBack) {
+                cardBack.style.opacity = 1;
+            });
+        }, 1500);
+        
+    } else {
+        revealed += selected.length;
+    }
+
+    return revealed;
+}
+
+function cardValid(card) {
+    return (card.className === 'back' && card.style.opacity === '1');
+}
+
+function victory(clicks) {
+    let newGame = setTimeout(function() {
+        confirm("You've won!\nIt took you " + clicks + " clicks\n\n Did you want to play again?");
+    }, 1500);
+    console.log(newGame);
+}
+
+function playGame(sets, setSize) {
+    let deck = createDeck(sets, setSize);
+    let surface = document.getElementById('surface');
+    let selected = [];
+    let revealed = 0;
+    let clicks = 0;
+    dealDeck(deck);
+    surface.addEventListener('click', function(event) {
+        clicks++;
+        let cardBack = event.target;
+
+        if (cardValid(cardBack)) {
+            cardBack.style.opacity = 0;
+            selected.push(cardBack);
+        }
+
+        if (selected.length === setSize) {
+            revealed = resolveSelected(selected, revealed);
+            selected = [];
+
+        }
+
+        if (revealed === sets * setSize) {
+            victory(clicks);
+        }
+    });
+}
+
+console.log('Scripts loaded');
+// document.getElementById("myElement").onclick = function() { return false; } 
